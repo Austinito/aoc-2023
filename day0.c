b85cb83af74f47328826884b83358fbe7213a0cf
Austinito <austinsprimary@gmail.com>
2023-12-03 20:07:29 -0800

    Okay, we got the hello world.

---
+++ day0.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    printf("Hello, World!\n");
+
+    printf("argc = %d\n", argc);
+    printf("argv[1] = %s\n", argv[1]);
+    return 0;
+}
